1558647b50d4946f5d2e4a6ad104ceb7e35c0c66
pierreosselin <pierre.osselin@gmail.com>
2020-04-22 20:08:44 +0200

    Add Gaussian Kernel

--- kernel.py
+++ kernel.py
@@ -20,3 +20,26 @@ class Kernel:
         Implementation of the specific covariance function for the kernel
         """
         pass
+
+class GaussianKernel(Kernel):
+    """
+    Kernel General Class
+    :return : Kernel Object
+    """
+    def __init__(self, lengthscale):
+        super().__init__()
+        self.lengthscale = lengthscale
+        pass
+
+    def build_covariance(self):
+        """
+        Build the cocvariance matrix for inference
+        :return : Covariance Matrix
+        """
+        pass
+
+    def covariance_functions(self):
+        """
+        Implementation of the specific covariance function for the kernel
+        """
+        pass
--- model.py
+++ model.py
@@ -1,12 +1,16 @@
 ## Model construction of the deep gaussian Processes
+import numpy as np
 
 class DeepGP:
     """
     Deep Gaussian Processes main class
     """
-    def __init__(self,nb_layers, listkernels):
-        self.nb_layers = nb_layers
+    def __init__(self,layers, listkernels):
+        self.layers = layers #list couple (beg, end)
         self.listkernels = listkernels
 
     def inference(self, points):
-        pass
+        n = points.shape[0]
+        for c, kernel in zip(self.layers, self.listkernels):
+            points = np.random.multivariate_normal(np.zeros(n) , kernel.build_covariance(points))
+        return points
